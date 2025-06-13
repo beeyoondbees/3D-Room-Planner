fa73a796c341522b98df32d1c477ff9cccd64bbb
Santhana Sanjay <sansanjay@Santhanas-MacBook-Air-2.local>
2025-06-13 12:24:27 +0530

    ar methods

--- 3D-Room-Planner/src/three/ARManager.js
+++ 3D-Room-Planner/src/three/ARManager.js
@@ -1,8 +1,10 @@
 // src/three/ARManager.js
 export function generateQR(modelName) {
-    console.log("generateQR called for model:", modelName);
-  const arViewerUrl = `https://3d-room-planner-s3.s3.eu-north-1.amazonaws.com/ar_viewer.html?model=${modelName}`;
-  const qrUrl = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${encodeURIComponent(arViewerUrl)}`;
+  console.log("generateQR called for model:", modelName);
+
+  // Replace this with your actual CloudFront distribution domain
+  const redirectUrl = `https://d3hu0a43t9wj79.cloudfront.net/ar_redirect.html?model=${modelName}`;
+  const qrUrl = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${encodeURIComponent(redirectUrl)}`;
 
   const modal = document.getElementById("arQrModal");
   const image = document.getElementById("arQrImage");
@@ -14,3 +16,4 @@ export function generateQR(modelName) {
     console.warn("QR modal elements not found in DOM.");
   }
 }
+
