88613cc36d3969416f782970726f8d7f208ced56
beeyoondbees <beeyoondbees@gmail.com>
2025-06-16 11:23:58 +0530

    change glb name3

--- 3D-Room-Planner/src/config/equipment.js
+++ 3D-Room-Planner/src/config/equipment.js
@@ -142,7 +142,7 @@ const equipmentConfig = {
         },
         {
           id: "PROwer-Rowing-Machine",
-          name: "PROwer-rowing-machine",
+          name: "PROwer-Rowing-Machine",
           icon: "/assets/icons/products/PROwer-Rowing-Machine.webp"
         }
       ]
